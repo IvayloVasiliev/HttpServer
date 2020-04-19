fb280e46df5ba06303282e7cd5a5f0e625487687
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-04-19 22:30:07 +0300

    Implemented support for master view/layout

--- Demo.Data/DatabaseConfiguration.cs
+++ Demo.Data/DatabaseConfiguration.cs
@@ -3,6 +3,6 @@
     public class DatabaseConfiguration
     {
         public const string ConnectionString = "Server=DESKTOP-4Q4QNM5\\SQLEXPRESS;Database=IRunesDB;Integrated Security=True;";
-
+        
     }
 }
